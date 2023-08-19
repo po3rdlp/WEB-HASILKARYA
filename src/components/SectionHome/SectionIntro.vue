5097ed07807ed19c1b18b05b4d0bc425b7986928
po3rdlp <leonpolandos53@gmail.com>
2023-08-19 16:17:18 +0800

    update:

--- src/components/SectionHome/SectionIntro.vue
+++ src/components/SectionHome/SectionIntro.vue
@@ -1,32 +1,59 @@
 <template>
-  <div class="hero min-h-screen bg-[#FFF2DF]">
-    <img
-      src="../../assets/img/emojione-monotone_sheaf-of-rice.png"
-      class="w-[65em] h-[30em] -left-9 -rotate-12 absolute z-50"
-    />
-    <div class="hero flex justify-between bg-black">
-      <div class="max-w-lg p-8 rounded-r-3xl">
-        <h1 class="text-5xl font-bold text-[#473D2A]">
-          Lebih baik, beragam pilihan dan lebih bernutrisi
-        </h1>
-        <p class="py-6 text-white">
-          Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi
-          exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.
-        </p>
-        <button class="btn btn-primary">Get Started</button>
-      </div>
-      <div class="rounded-tl-3xl">
-        <div class="w-[32em] h-full absolute right-0 bottom-0">
-          <img
-            src="../../assets/img/Background.png"
-            class="w-full h-[25em] bottom-0 right-28 absolute"
-          />
-          <img
-            src="../../assets/img/sack-rice-with-rice-wooden-spoon-rice-plant 1.png"
-            class="w-full h-[33em] bottom-0 absolute"
-          />
+  <section class="flex items-end min-h-screen bg-[#FFF2DF]">
+    <div class="absolute -left-1 -bottom-96">
+      <img
+        src="../../assets/img/emojione-monotone_sheaf-of-rice.png"
+        class="max-w-full h-[70rem]"
+      />
+    </div>
+    <div class="flex justify-start w-full h-full">
+      <div class="px-5 py-10">
+        <div>
+          <div class="max-w-lg p-8 rounded-r-3xl">
+            <h1 class="text-5xl font-bold text-[#473D2A]">
+              Lebih baik, beragam pilihan dan lebih bernutrisi
+            </h1>
+            <p class="py-6 text-black">
+              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi
+              exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.
+            </p>
+            <button class="btn btn-primary">TENTANG KAMI</button>
+          </div>
+        </div>
+        <div class="hidden lg:mt-0 lg:col-span-5 lg:flex">
+          <div class="rounded-tl-3xl">
+            <div class="w-[28em] h-full absolute right-0 bottom-0">
+              <img
+                src="../../assets/img/Background.png"
+                class="w-full h-[25em] bottom-0 right-28 absolute"
+              />
+              <img
+                src="../../assets/img/sack-rice-with-rice-wooden-spoon-rice-plant 1.png"
+                class="w-full h-[33em] bottom-0 absolute"
+              />
+            </div>
+          </div>
         </div>
       </div>
     </div>
-  </div>
+    <div class="flex justify-center w-full -bottom-7 absolute">
+      <div class="flex justify-center items-center space-x-7 bg-white shadow-2xl rounded-lg">
+        <div class="w-1/5 p-3">
+          <img src="../../assets/img/LOGO/multimart.png" alt="Logo" />
+        </div>
+        <div class="w-1/5 p-3">
+          <img src="../../assets/img/LOGO/alfamart.png" alt="Logo" />
+        </div>
+        <div class="w-1/5 p-3">
+          <img src="../../assets/img/LOGO/indomart.png" alt="Logo" />
+        </div>
+        <div class="w-1/5 p-3">
+          <img src="../../assets/img/LOGO/indgrosir.png" alt="Logo" />
+        </div>
+        <div class="w-1/5 p-3">
+          <img src="../../assets/img/LOGO/transmart.png" alt="Logo" />
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,8 +2,12 @@
   <div>
     <SectionIntro />
   </div>
+  <div>
+    <SectionBackground />
+  </div>
 </template>
 
 <script setup>
 import SectionIntro from '../components/SectionHome/SectionIntro.vue'
+import SectionBackground from '../components/SectionHome/SectionBackground.vue'
 </script>
