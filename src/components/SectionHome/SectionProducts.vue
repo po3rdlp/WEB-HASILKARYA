b4ef47889c058fe36a53bd606e9ff779c2c5dfb2
po3rdlp <leonpolandos53@gmail.com>
2023-08-22 00:29:41 +0800

    update: SectionProduct(NeedFix)

---
+++ src/components/SectionHome/SectionProducts.vue
@@ -0,0 +1,86 @@
+<template>
+  <div class="w-full h-full p-1 lg:px-5 lg:py-5 lg:p-3 bg-[#F38630]">
+    <div class="flex gap-2 lg:gap-5 overflow-x-auto">
+      <div class="card w-full h-full bg-base-100 rounded-3xl relative">
+        <div class="flex justify-end p-2">
+          <IconHeartFilled class="z-10 text-red-500" />
+        </div>
+        <figure>
+          <img
+            src="../../assets/img/Products/Products.png"
+            class="w-full absolute top-0 h-full"
+            alt="Shoes"
+          />
+          <img src="../../assets/img/Products/Dua_Merpati_5kg.png" class="max-w-xs" alt="Shoes" />
+        </figure>
+        <div class="p-3 z-10">
+          <div class="flex justify-between">
+            <p class="text-white">BERAS DUA MERPATI 5KG</p>
+            <button class="btn btn-primary z-10">Pesan</button>
+          </div>
+        </div>
+      </div>
+      <div class="card w-full h-full bg-base-100 rounded-3xl relative">
+        <div class="flex justify-end p-2">
+          <IconHeartFilled class="z-10 text-red-500" />
+        </div>
+        <figure>
+          <img
+            src="../../assets/img/Products/Products.png"
+            class="w-full absolute top-0 h-full"
+            alt="Shoes"
+          />
+          <img src="../../assets/img/Products/Dua_Merpati_5kg.png" class="max-w-xs" alt="Shoes" />
+        </figure>
+        <div class="p-3 z-10">
+          <div class="flex justify-between">
+            <p class="text-white">BERAS DUA MERPATI 5KG</p>
+            <button class="btn btn-primary z-10">Pesan</button>
+          </div>
+        </div>
+      </div>
+      <div class="card w-full h-full bg-base-100 rounded-3xl relative">
+        <div class="flex justify-end p-2">
+          <IconHeartFilled class="z-10 text-red-500" />
+        </div>
+        <figure>
+          <img
+            src="../../assets/img/Products/Products.png"
+            class="w-full absolute top-0 h-full"
+            alt="Shoes"
+          />
+          <img src="../../assets/img/Products/Dua_Merpati_5kg.png" class="max-w-xs" alt="Shoes" />
+        </figure>
+        <div class="p-3 z-10">
+          <div class="flex justify-between">
+            <p class="text-white">BERAS DUA MERPATI 5KG</p>
+            <button class="btn btn-primary z-10">Pesan</button>
+          </div>
+        </div>
+      </div>
+      <div class="card w-full h-full bg-base-100 rounded-3xl relative">
+        <div class="flex justify-end p-2">
+          <IconHeartFilled class="z-10 text-red-500" />
+        </div>
+        <figure>
+          <img
+            src="../../assets/img/Products/Products.png"
+            class="w-full absolute top-0 h-full"
+            alt="Shoes"
+          />
+          <img src="../../assets/img/Products/Dua_Merpati_5kg.png" class="max-w-xs" alt="Shoes" />
+        </figure>
+        <div class="p-3 z-10">
+          <div class="flex justify-between">
+            <p class="text-white">BERAS DUA MERPATI 5KG</p>
+            <button class="btn btn-primary z-10">Pesan</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { IconHeartFilled } from '@tabler/icons-vue'
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,9 +5,13 @@
   <div>
     <SectionBackground />
   </div>
+  <div>
+    <SectionProducts />
+  </div>
 </template>
 
 <script setup>
 import SectionIntro from '../components/SectionHome/SectionIntro.vue'
 import SectionBackground from '../components/SectionHome/SectionBackground.vue'
+import SectionProducts from '../components/SectionHome/SectionProducts.vue'
 </script>
